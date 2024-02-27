fdb1a9af8d247d363d9d3f36c5d5f6e8cfc9e499
Silongo Vailolo <silongo68@hotmail.com>
2024-02-27 14:36:06 +1000

    Added Change Player function and check Winner Function.

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const winConditions = [
   [0, 4, 8],
   [2, 4, 6]
 ];
-let options = ["","","","","","","","",""];
+let options = ["", "", "", "", "", "", "", "", ""];
 let currentPlayer= "X";
 let running = false;
 
@@ -21,30 +21,61 @@ function initializeGame(){
   cells.forEach(cell => cell.addEventListener("click", cellClicked))
   restartBtn.addEventListener("click", restartGame);
   statusText.textContent = `${currentPlayer}'s Turn`;
+  running = true;
 }
 
 //Kai
 function cellClicked(){
-const cellindex =this.getAttribute("cellindex");
+const cellIndex =this.getAttribute("cellIndex");
 
-if (options[cellindex] != "" || !running){
-   return
+if (options[cellIndex] != "" || !running){
+   return;
 }
 
-updateCell(this, cellindex);
+updateCell(this, cellIndex);
+changePlayer();
 checkWinner();
 }
 
-function updateCell(){
+function updateCell(cell, index){
   options[index] = currentPlayer;
   cell.textContent = currentPlayer;
 }
 
 function changePlayer(){
-
+  currentPlayer = (currentPlayer == "X") ? "O" : "X";
+  statusText.textContent = `${currentPlayer}'s Turn`;
 }
 function checkWinner(){
+    let roundwon = false;
 
+    for(let i= 0; i < winConditions.length; i++){
+        const condition = winConditions[i];
+        const cellA = options[condtion[0]];
+        const cellB = options[condtion[1]];
+        const cellC = options[condtion[2]];
+      
+      if(cellA == "" || cellB == "" || cellC == ""){
+        continue;
+      }
+      if(cellA == cellB && cellB == cellC){
+        roundwon = true;
+        break
+      }
+
+      if(roundwon){
+        statusText.textContent = `${currentPlayer} Wins!`;
+        running = false;
+      }
+      else if(options.includes("")){
+        statusText.textContent = `Draw!`;
+        running = false;
+      }
+      else{
+        changePlayer();
+      }
+
+}
 }
 
 function restartGame(){
